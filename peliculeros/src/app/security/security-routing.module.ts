3b5efecf09406cd0dcb03fdf909fbfd2b149eca9
derschrei <cnunezsancho@gmail.com>
2017-04-05 13:52:26 +0200

    Fixed routes

--- peliculeros/src/app/security/security-routing.module.ts
+++ peliculeros/src/app/security/security-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { SecurityComponent } from './security/security.component';
 
 const routes: Routes = [];
+export const routableComponents = [
+  LoginComponent,
+  SecurityComponent
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
