31b715a4ae53108b4ef67aabbeda1f9a336ebcf4
Formacion609 <Formacion609@stkproj.com>
2017-04-05 14:26:23 +0200

    app works!

---
+++ peliculeros/src/app/app-routing.module.ts
@@ -0,0 +1,29 @@
+/** Módulos de enrutado de Angular2 */
+import { RouterModule, Routes } from '@angular/router';
+
+import { NgModule } from '@angular/core';
+
+// Array con las rutas de este módulo
+const routes: Routes = [
+  { path: 'home', redirectTo: '' }
+//   { path: '', loadChildren: './casa/casa.module#CasaModule'},
+//   { path: 'movimientos', loadChildren: './movimientos/movimientos.module#MovimientosModule'},
+//   { path: 'contacto', loadChildren: './contacto/contacto.module#ContactoModule'}
+  // { path: 'contacto', redirectTo: '' },
+  /*{
+    path: '**', // para casos not-found
+    redirectTo: '',
+    pathMatch: 'full'
+  }*/
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes) // configuración para un módulo raiz
+  ],
+  exports: [
+    RouterModule // se importará desde el módulo padre, el raíz
+  ]
+})
+
+export class AppRoutingModule { }
--- peliculeros/src/app/app.module.ts
+++ peliculeros/src/app/app.module.ts
@@ -1,20 +1,9 @@
-import { BrowserModule } from '@angular/platform-browser';
+import * as app from './index';
+
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
 
-import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
 
-@NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
+@NgModule(app.appConfig)
+
 export class AppModule { }
